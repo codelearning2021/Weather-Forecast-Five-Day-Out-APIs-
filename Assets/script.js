e4a80cfe033ed81112cb2ea526bccfea103ae522
Nate J <nathan.evan.johnson@gmail.com>
2023-04-24 09:59:39 -0700

    Made some necessary changes to how session storage is handled

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,5 @@
 // Declare site variables and key
-  const API_KEY = '6941df9547eafe90e5ca91d9d5f9a4e8'; // replace with your API key
+const API_KEY = '6941df9547eafe90e5ca91d9d5f9a4e8'; // replace with your API key
 document.getElementById("citylist").addEventListener("click", (event) => {
   console.log(event.target);
   console.log(event.target.matches("li"))
@@ -7,17 +7,84 @@ document.getElementById("citylist").addEventListener("click", (event) => {
     const cityName = event.target.textContent;
     console.log(cityName);
     addToList(cityName);
+    saveHistory(cityName);
   }
 })
 
+function saveHistory(cityName) {
+  let storage = JSON.parse(localStorage.getItem("weatherHistory"))
+  if (storage === null) {
+    storage = []
+
+  }
+  
+  storage.push (cityName)
+  localStorage.setItem ("weatherHistory", JSON.stringify (storage))
+  getHistory ()
+}
+
+function getHistory () {
+  let storage = JSON.parse(localStorage.getItem("weatherHistory"))
+  if (storage === null) {
+    document.getElementById("citydiv").textContent="no history"
+  } else {
+    document.getElementById("citydiv").textContent=""
+    for(let i=0; i<storage.length; i++) {
+      // create buttons and add text content to storage[i], then append to "citydiv" div
+      let button = document.createElement("button");
+    button.textContent = storage;
+    document.getElementById("citydiv").appendChild(button);
+    }
+  }
+}
+
 function getFiveDaysWeather(lat, lon) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
-  fetch (url)
+  fetch(url)
     .then(response => response.json())
-    .then(data => {
-      console.log(data)
+    .then(fullWeather => {
+      console.log(fullWeather);
+      var foreCastTemp1 = fullWeather.list[0].main.temp;
+      var foreCastHumid1 = fullWeather.list[0].main.humidity;
+      // $("#city-five-icon-one").attr("src", cityFiveIconOne);
+      var foreCastWind1 = fullWeather.list[0].wind.speed;
+      $("#callTemp1").text(foreCastTemp1);
+      $("#callWind1").text(foreCastWind1);
+      $("#callHumid1").text(foreCastHumid1);
+
+      var foreCastTemp2 = fullWeather.list[8].main.temp;
+      var foreCastHumid2 = fullWeather.list[8].main.humidity;
+      // $("#city-five-icon-one").attr("src", cityFiveIconOne);
+      var foreCastWind2 = fullWeather.list[8].wind.speed;
+      $("#callTemp2").text(foreCastTemp2);
+      $("#callWind2").text(foreCastWind2);
+      $("#callHumid2").text(foreCastHumid2);
+
+      var foreCastTemp3 = fullWeather.list[16].main.temp;
+      var foreCastHumid3 = fullWeather.list[16].main.humidity;
+      // $("#city-five-icon-one").attr("src", cityFiveIconOne);
+      var foreCastWind3 = fullWeather.list[16].wind.speed;
+      $("#callTemp3").text(foreCastTemp3);
+      $("#callWind3").text(foreCastWind3);
+      $("#callHumid3").text(foreCastHumid3);
+
+      var foreCastTemp4 = fullWeather.list[24].main.temp;
+      var foreCastHumid4 = fullWeather.list[24].main.humidity;
+      // $("#city-five-icon-one").attr("src", cityFiveIconOne);
+      var foreCastWind4 = fullWeather.list[24].wind.speed;
+      $("#callTemp4").text(foreCastTemp4);
+      $("#callWind4").text(foreCastWind4);
+      $("#callHumid4").text(foreCastHumid4);
+
+      var foreCastTemp5 = fullWeather.list[32].main.temp;
+      var foreCastHumid5 = fullWeather.list[32].main.humidity;
+      // $("#city-five-icon-one").attr("src", cityFiveIconOne);
+      var foreCastWind5 = fullWeather.list[32].wind.speed;
+      $("#callTemp5").text(foreCastTemp5);
+      $("#callWind5").text(foreCastWind5);
+      $("#callHumid5").text(foreCastHumid5);
     }
-      )
+    )
 }
 
 
@@ -33,234 +100,115 @@ function addToList(cityName) {
     li.textContent = city + ' ';
     document.getElementById("citylist").appendChild(li);
   }
-    
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`;
-    
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
 
-        const weatherDescription = data.weather[0].description;
-        const temperatureK = data.main.temp;
-        const temperatureF = (temperatureK * 9/5) - 459.67;
-        console.log(`The weather in ${CITY} is ${weatherDescription} with a temperature of ${temperatureF.toFixed(1)} °F.`);
-        var wD = "The weather in " + CITY + " is " + weatherDescription + " with a temperature of " + temperatureF.toFixed(1) + " °F.";
-        document.getElementById("weatherDetails").innerHTML = wD;
-        console.log(data);
-        getFiveDaysWeather(data.coord.lat, data.coord.lon)
-      })
-      .catch(error => console.error('Error fetching weather data:', error));
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}`;
 
-$.ajax({
-  url: url,
-  method: "GET"
-}).then(function(fullWeather) {
-console.log(fullWeather);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+      const weatherDescription = data.weather[0].description;
+      const temperatureK = data.main.temp;
+      const temperatureF = (temperatureK * 9 / 5) - 459.67;
+      console.log(`The weather in ${CITY} is ${weatherDescription} with a temperature of ${temperatureF.toFixed(1)} °F.`);
+      var wD = "The weather in " + CITY + " is " + weatherDescription + " with a temperature of " + temperatureF.toFixed(1) + " °F.";
+      document.getElementById("weatherDetails").innerHTML = wD;
+      console.log(data);
+      getFiveDaysWeather(data.coord.lat, data.coord.lon)
+    })
+    .catch(error => console.error('Error fetching weather data:', error));
 
 
 
-    // Getting API values for search city section and append them to the display
-    // var selectCity = fullWeather.list[0].name;
-    // var selectIcon = "https://openweathermap.org/img/w/" + fullWeather.list[0].weather[0].icon + ".png";
-    // var selectTemp = fullWeather.list[0].main.temp;
-    // var selectHumid = fullWeather.list[0].main.humidity;
-    // var selectWind = fullWeather.list[0].wind.speed;
-    // $("#select-area").text(selectCity);
-    // $("#select-icon").attr("src", selectIcon);
-    // $("#select-temp").text(selectTemp);
-    // $("#select-humid").text(selectHumid);
-    // $("#select-wind").text(selectWind);
+
+
+  // Getting API values for search city section 
+  // $("#select-wind").text(selectWind);
+
+  $.ajax({
+    url: url,
+    method: "GET"
+  }).then(function (fullWeather) {
+    console.log(fullWeather);
+
+    // Setting weather values for every day/block
+
+    // Day 1
+    var cityFiveDateOne = fullWeather.list[0].dt_txt;
+
+    var cityFiveIconOne = "https://openweathermap.org/img/w/" + fullWeather.list[0].weather[0].icon + ".png";
+    var foreCastTemp1 = fullWeather.list[0].main.temp;
+    var foreCastHumid1 = fullWeather.list[0].main.humidity;
+    $("#city-five-icon-one").attr("src", cityFiveIconOne);
+    var foreCastWind1 = fullWeather.list[0].wind.speed;
+    $("#callTemp1").text(foreCastTemp1);
+    $("#callWind1").text(foreCastWind1);
+    $("#callHumid1").text(foreCastHumid1);
+
+    // Day 2
+    var cityFiveDateTwo = fullWeather.list[8].dt_txt;
+
+    var cityFiveIconTwo = "https://openweathermap.org/img/w/" + fullWeather.list[8].weather[8].icon + ".png";
+    var foreCastTemp2 = fullWeather.list[8].main.temp;
+    var foreCastHumid2 = fullWeather.list[8].main.humidity;
+    $("#city-five-icon-two").attr("src", cityFiveIconTwo);
+    var foreCastWind2 = fullWeather.list[8].wind.speed;
+    $("#callTemp2").text(foreCastTemp2);
+    $("#callWind2").text(foreCastWind2);
+    $("#callHumid2").text(foreCastHumid2);
+
+    // Day 3
+    var cityFiveDateThree = fullWeather.list[16].dt_txt;
+
+    var cityFiveIconThree = "https://openweathermap.org/img/w/" + fullWeather.list[16].weather[16].icon + ".png";
+    var foreCastTemp3 = fullWeather.list[16].main.temp;
+    var foreCastHumid3 = fullWeather.list[16].main.humidity;
+    $("#city-five-icon-three").attr("src", cityFiveIconThree);
+    var foreCastWind3 = fullWeather.list[16].wind.speed;
+    $("#callTemp3").text(foreCastTemp3);
+    $("#callWind3").text(foreCastWind3);
+    $("#callHumid3").text(foreCastHumid3);
+
+    // Day 4
+
+    var cityFiveDateFour = fullWeather.list[24].dt_txt;
+
+    var cityFiveIconFour = "https://openweathermap.org/img/w/" + fullWeather.list[24].weather[24].icon + ".png";
+    var foreCastTemp4 = fullWeather.list[24].main.temp;
+    var foreCastHumid4 = fullWeather.list[24].main.humidity;
+    $("#city-five-icon-four").attr("src", cityFiveIconFour);
+    var foreCastWind4 = fullWeather.list[24].wind.speed;
+    $("#callTemp4").text(foreCastTemp4);
+    $("#callWind4").text(foreCastWind4);
+    $("#callHumid4").text(foreCastHumid4);
+
+    // Day 5
+    var cityFiveDateFive = fullWeather.list[32].dt_txt;
+
+    var cityFiveIconFive = "https://openweathermap.org/img/w/" + fullWeather.list[32].weather[32].icon + ".png";
+    var foreCastTemp5 = fullWeather.list[32].main.temp;
+    var foreCastHumid5 = fullWeather.list[32].main.humidity;
+    $("#city-five-icon-five").attr("src", cityFiveIconfive);
+    var foreCastWind5 = fullWeather.list[32].wind.speed;
+    $("#callTemp5").text(foreCastTemp5);
+    $("#callWind5").text(foreCastWind5);
+    $("#callHumid5").text(foreCastHumid5);
+  })
+}
 
 
 
-   // Setting weather values for every day/block
+var storage = JSON.parse(sessionStorage.getItem('quizHighscores'))
+var highscoresEl = document.querySelector(".highscores")
 
-   // Day 1
-   var cityFiveDateOne = fullWeather.list[1].dt_txt;
-   // var cityFiveIconOne = "https://openweathermap.org/img/w/" + fullWeather.list[0].weather[0].icon + ".png";
-   var foreCastTemp1 = fullWeather.list[0].main.temp;
-   var foreCastHumid1 = fullWeather.list[0].main.humidity;
-   // $("#city-five-icon-one").attr("src", cityFiveIconOne);
-   var foreCastWind1 = fullWeather.list[0].wind.speed;
-   $("#callTemp1").text(foreCastTemp1); 
-   $("#callWind1").text(foreCastWind1); 
-   $("#callHumid1").text(foreCastHumid1);
+if (storage === null) {
+    highscoresEl.textContent = "No Highscores"
+} else {
+    highscoresEl.textContent = ''
 
-})}
-
-// const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
-// const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
-// const FORECAST = document.getElementsByClassName('component__forecast-box')[0];
-
-// const URL = "https://api.openweathermap.org/data/2.5/forecast/daily?" +
-//     "q=CITY&cnt=7&units=imperial&APPID=6941df9547eafe90e5ca91d9d5f9a4e8";
-
-// const URLcoordinates = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=6941df9547eafe90e5ca91d9d5f9a4e8"
-
-// // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-// // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
-
-
-
-// function getWeatherData() {
-//     let headers = new Headers();
-
-//     return fetch(URLcoordinates, {
-//         method: 'GET',
-//         headers: headers
-//     }).then(data => {
-//         return data.json();
-//     });
-//     console.log(data);
-// }
-
-// // one api method
-// // const client = require('@bigdatacloudapi/client')(API_KEY);
-
-// // async foo() {
-// //     ...
-// //     const location: string = await client.getReverseGeocode({
-// //           latitude:'32.101786566878445', 
-// //           longitude: '34.858965073072056'
-// //     });
-// // }
-
-
-// function getAddress (latitude, longitude) {
-//     return new Promise(function (resolve, reject) {
-//         var request = new XMLHttpRequest();
-
-//         var method = 'GET';
-//         var url = 'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&sensor=true';
-//         var async = true;
-
-//         request.open(method, url, async);
-//         request.onreadystatechange = function () {
-//             if (request.readyState == 4) {
-//                 if (request.status == 200) {
-//                     var data = JSON.parse(request.responseText);
-//                     var address = data.results[0];
-//                     resolve(address);
-//                 }
-//                 else {
-//                     reject(request.status);
-//                 }
-//             }
-//             console.log(data);
-//         };
-//         request.send();
-//     });
-// };
-// getAddress(lat, lon).then(console.log).catch(console.error);
-
-
-// getWeatherData().then(weatherData => {
-//     let city = weatherData.city.name;
-//     let dailyForecast = weatherData.list;
-
-//     renderData(city, dailyForecast);
-// });
-
-// renderData = (location, forecast) => {
-//     const currentWeather = forecast[0].weather[0];
-//     const widgetHeader =
-//         `<h1>${location}</h1><small>${currentWeather.description}</small>`;
-
-//     CURRENT_TEMP.innerHTML =
-//         `<i class="wi ${applyIcon(currentWeather.icon)}"></i>
-//     ${Math.round(forecast[0].temp.day)} <i class="wi wi-degrees"></i>`;
-
-//     CURRENT_LOCATION.innerHTML = widgetHeader;
-
-//     // render each daily forecast
-//     forecast.forEach(day => {
-//         let date = new Date(day.dt * 1000);
-//         let days = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
-//         let name = days[date.getDay()];
-//         let dayBlock = document.createElement("div");
-//         dayBlock.className = 'forecast__item';
-//         dayBlock.innerHTML =
-//             `<div class="forecast-item__heading">${name}</div>
-//         <div class="forecast-item__info">
-//         <i class="wi ${applyIcon(day.weather[0].icon)}"></i>
-//         <span class="degrees">${Math.round(day.temp.day)}
-//         <i class="wi wi-degrees"></i></span></div>`;
-//         FORECAST.appendChild(dayBlock);
-//     });
-// }
-
-// // uses API ninja geocoding to translate coordinates from Google API geocoding
-
-// var city = citySubmit // 'london' as an example
-// $.ajax({
-//     method: 'GET',
-//     url: 'https://api.api-ninjas.com/v1/geocoding?city=' + city,
-//     headers: { 'X-Api-Key': 'Y6Yvsu+zALgibJQrcH/n8g==WfOrKO3OzlGnKgt0' },
-//     contentType: 'application/json',
-//     success: function (result) {
-//         console.log(result);
-//     },
-//     error: function ajaxError(jqXHR) {
-//         console.error('Error: ', jqXHR.responseText);
-//     }
-// });
-
-
-// // Sample output:
-// // [
-// //     {
-// //         "name": "London",
-// //         "latitude": 51.5085,
-// //         "longitude": -0.1257,
-// //         "country": "GB"
-// //     }
-// // ]
-
-// document
-//     .getElementById("submitcity")
-//     .addEventListener("click", async function () {
-//         var citySubmit = document.getElementById("citysubmit").value;
-//         console.log(citySubmit);
-//     });
-
-// // async function fetchData(weight, height) {
-// //   const options = {
-// //     method: "GET",
-// //     headers: {
-// //       "X-RapidAPI-Key": "c102ffcecemsh31fee262b485c63p1b464fjsn535ea8fa7b4e",
-// //       "X-RapidAPI-Host": "mega-fitness-calculator1.p.rapidapi.com",
-// //     },
-// //   };
-// //   fetch(
-// //     `https://mega-fitness-calculator1.p.rapidapi.com/bmi?weight=${weight}&height=${height}`,
-// //     options
-// //   )
-// //     .then((response) => response.json())
-// //     .then((data) => {
-// //       var BMIel = document.createElement("p");
-// //       BMIel.textContent = "Your BMI: " + data.info.bmi;
-// //       var HEALTHel = document.createElement("p");
-// //       HEALTHel.textContent = "Health Category: " + data.info.health;
-// //       var RANGE = document.createElement("p");
-// //       RANGE.textContent = "Healthy BMI Range: " + data.info.healthy_bmi_range;
-// //       var card = document.createElement("article");
-// //       card.setAttribute("class", "card1");
-// //       var output = document.querySelector("#outputBmi");
-// //       card.append(BMIel, HEALTHel, RANGE);
-// //       output.append(card);
-// //       console.log(data);
-// //     })
-// //     .catch((err) => console.error(err));
-// // }
-// // var API_URL = "https://mega-fitness-calculator1.p.rapidapi.com/";
-// // var API_KEY = "c102ffcecemsh31fee262b485c63p1b464fjsn535ea8fa7b4e";
-// // async function fetchAllData(endpoint) {
-// //   var url = `${API_URL}${endpoint}`;
-// //   var options = {
-// //     method: "GET",
-// //     headers: {
-// //       "X-RapidAPI-Key": API_KEY,
-// //     },
-// //   };
-// // }
-
+    for (var i = 0; i < storage.length; i++) {
+        var p = document.createElement('p')
+        p.textContent = `Name: ${storage[i].name} ---------- Score: ${storage[i].score}`
+        highscoresEl.append(p)
+    }
+}
